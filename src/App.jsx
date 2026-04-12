e5b94a092d629d8e45c16bfd7aca593081711d69
Qasim <mianqasim065@gmail.com>
2026-04-12 11:25:57 +0500

    setup project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return <div class="border border-red-600 text-5xl">App</div>;
+  }
+}
+
+export default App;

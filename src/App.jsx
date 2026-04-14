8aca55f8716c69067a340940d191f0c133fad15e
Qasim <mianqasim065@gmail.com>
2026-04-14 14:03:16 +0500

    feat(heroSection): create a basic statis hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+
 function App() {
   return (
     <main>
       <Navbar />
+      <HeroSection />
     </main>
   );
 }

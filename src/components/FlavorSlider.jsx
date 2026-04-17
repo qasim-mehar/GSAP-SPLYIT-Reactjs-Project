fd0500432d7f3f47860ab49c50562b32c6e65d1e
Qasim <mianqasim065@gmail.com>
2026-04-17 19:39:32 +0500

    feat(flavors): build flavor slider component structure

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -1,5 +1,34 @@
+import { flavorlists } from "../constants/index";
 function FlavorSlider() {
-  return <div></div>;
+  return (
+    <div className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+          <div
+            key={flavor.name}
+            className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${flavor.rotation}`}
+          >
+            <img
+              src={`/images/${flavor.color}-bg.svg`}
+              alt=""
+              className="absolute bottom-0"
+            />
+            <img
+              src={`/images/${flavor.color}-drink.webp`}
+              alt=""
+              className="drinks"
+            />
+            <img
+              src={`/images/${flavor.color}-elements.webp`}
+              alt=""
+              className="elements"
+            />
+            <h1>{flavor.name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default FlavorSlider;
